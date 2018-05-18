79fb2e30b7ad80384634f395f94320ecb98c48df
Krzysztof-Gwizdz <gwizdzu1@gmail.com>
2018-05-19 01:14:49 +0200

    Podejscie pierwsze do GUI

--- src/charactercreator/CharacterCreator.java
+++ src/charactercreator/CharacterCreator.java
@@ -5,17 +5,33 @@
  */
 package charactercreator;
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Krzysztof Gwiżdż
  */
 public class CharacterCreator {
+    
+    public class WindowFrame extends JFrame {
+        public WindowFrame(){
+            super("CCCreator");
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setVisible(true);
+        }
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+       EventQueue.invokeLater(new Runnable(){
+           @Override
+           public void run(){
+               new WindowFrame();
+           }
+       });
     }
     
 }
